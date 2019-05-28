6f2b37cc99a74b66d1bb1d8b4d18d11bc9415219
pilkonimo <maxmkahl@gmail.com>
2019-05-28 19:36:06 +0200

    added documentation

--- 05_AlDa_UB_L/calculator.py
+++ 05_AlDa_UB_L/calculator.py
@@ -17,6 +17,7 @@ class Operator:
 
 
 def check_infix(infix):
+    """Checks if given infix expression is syntactically correct."""
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     operators = ['+', '-', '/', '*']
     parenth_counter = 0
@@ -52,48 +53,65 @@ def check_infix(infix):
 
 
 def convert_to_postfix(infix):
-    stack = []
+    """Converts infix expression into postfix expression of identical meaning."""
+    stack = []  # keep operators here
     postfix = ''
     precedence = {'*': 2, '/': 2, '+': 1, '-': 1, '(': 0}
 
     for c in infix:
+        # numbers can be added to postfix expression immediately otherwise one needs to differentiate
         if c not in ['+', '-', '/', '*', '(', ')']:
             postfix += c
         else:
+            # parentheses stay at their position only chars inside need to change order
             if c == '(':
                 stack.append(c)
+            # pop stack until corresponding open parentheses is found
             elif c == ')':
                 operator = stack.pop()
                 while not operator == '(':
                     postfix += operator
                     operator = stack.pop()
+            # push operator on stack unless operators with higher precedence are already on stack, these can be added
+            # to expression because higher ranked operators will always be left and the most left operator is the first
+            # to take effect
             else:
                 while len(stack) > 0 and precedence[stack[-1]] >= precedence[c]:
                     postfix += stack.pop()
                 stack.append(c)
 
+    # at the end add already sorted operators to expression
     while len(stack) > 0:
         postfix += stack.pop()
     return postfix
 
 
 def parse(s):
+    """Parses given infix expression into binary tree/syntax tree and returns root."""
     stack = []
     if not check_infix(s):
         raise SyntaxError('infix expression is incorrect!')
     postfix = convert_to_postfix(s)
 
+    # given a postfix expression you can create the corresponding syntax tree easily using a stack
+    # iterating through the postfix expression numbers can be pushed instantly
     for c in postfix:
         if c not in ['+', '-', '/', '*']:
             stack.append(Number(c, None, None))
+        # operators need to be applied to last two numbers of expression/stack, that means
+        # only operators are inner children which need to have operands as children/leaves
+        # the resulting subtree can again be a children for a new operator node, which is why it
+        # needs to be pushed to the stack
         else:
             right = stack.pop()
             left = stack.pop()
             stack.append(Operator(c, left, right))
+        # at the end only one node should be left - the root node
     return stack.pop()
 
 
 def operation(op1, operator, op2):
+    """Calculates simple arithmetic expression given operator as a string."""
     if operator == '+':
         return op1 + op2
     if operator == '-':
@@ -105,14 +123,19 @@ def operation(op1, operator, op2):
 
 
 def evaluate(root):
+    """Evaluates syntax tree using recursion."""
+    # if node is number return it
     if isinstance(root, Number):
         return root.number
+    # otherwise gain corresponding number to left and right subtree (recursion)
     operand_l = evaluate(root.left)
     operand_r = evaluate(root.right)
+    # and use operation corresponding to current operator node
     return operation(operand_l, root.operator, operand_r)
 
 
 class TestCalculator(unittest.TestCase):
+    """Testclass to test calculator on simple arithmetic expressions and cases with different operator precedences."""
     def test_addition(self):
         operand1 = random.randint(0, 9)
         operand2 = random.randint(0, 9)
